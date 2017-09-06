663d0050382d9dd889ea6f051840863136012efb
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2017-09-06 11:42:03 +0000

    Ignore anything starting with .git

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync . s3://wpmlorg/wpml-config/ --exclude "update.*" --exclude ".git/*" --delete
+aws s3 sync . s3://wpmlorg/wpml-config/ --exclude "update.*" --exclude ".git*" --delete
 
