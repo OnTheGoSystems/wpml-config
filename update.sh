9feb973e570beaf90ffea63534a09b5a27b52488
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2017-09-07 14:02:25 +0000

    Go live

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 rm -f config-index.json
 php update.php
 if [[ -f config-index.json ]]; then
-	aws s3 sync . s3://wpmldebug/wpml-config/ --exclude "update.*" --exclude ".git*" --delete --acl public-read
+	aws s3 sync . s3://wpmlorg/wpml-config/ --exclude "update.*" --exclude ".git*" --delete --acl public-read
 fi
